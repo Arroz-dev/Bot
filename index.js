7e9ee8331d82e6c6893e9e63d90a35ca2bd9f4bf
Arroz-dev <arrozpapu202123@gmail.com>
2024-08-25 20:21:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const {Client, LocalAuth} = require('whatsapp-wed.js')
 const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+//genera un codigo qr
+client.on('qr',qr=>{
+  qrcode.generate(qr,{Small: true});
+  console.log('El codigo QR fue generado, escanealo en tu whatsapp');
+});
+
+client.on('message',message=>{
+  console.log('mensaje recivido: ', message.body);
+  if(message.body == 'Hola'){
+    message.reply('Hola en que puedo ayudarte');
+  }
+});
+
+client.initialize();
